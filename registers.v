8550c4ae93c4ae8fa421efc27aed82703eb7fee8
Zhao Wang <jaz.wang@mail.utoronto.ca>
2018-11-27 20:05:35 -0500

    added registers

--- registers.v
+++ registers.v
@@ -1,8 +1,51 @@
 module PC (clock, in, out, LdPC, reset);
 	input clock;
 	input [15:0] in;
+	input LdPC;
+	input reset;
 	output [15:0] out;
+
+	reg [15:0] out;
 	
+	always @(posedge clock)
+	begin
+		if (reset)
+		begin
+			out <= 16'b0;
+		end
+		else if (LdPc)
+		begin
+			out <= in;
+		end
+	end
 	
+endmodule
+
+
+module BCount (clock, out, BCountDecInc, BCountEnable, reset);
+	input clock;
+	input BCountDecInc;
+	input BCountEnable;
+	input reset;
+	
+	output [7:0] out;
+	
+	reg [7:0] out;
+	
+	always @(posedge clock)
+	begin
+		if (reset)
+		begin
+			out <= 8'b0;
+		end
+		else if (BCountEnable & !BCountDecInc)
+		begin
+			out <= out + 8'b1;
+		end
+		else if (BCountEnable & BCountDecInc)
+		begin
+			out <= out - 8'b1;
+		end
+	end
 
 endmodule
